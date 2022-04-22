4d645678434c063fe46ac90d6a2727a9b41cd7f6
Nguyen Thanh Sy <nguyensy270395@gmail.com>
2022-04-22 10:57:02 +0700

    Initial Commit

---
+++ ScheduleAppSwiftUI/ScheduleAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScheduleAppSwiftUIApp.swift
+//  ScheduleAppSwiftUI
+//
+//  Created by Nguyễn Thanh Sỹ on 22/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct ScheduleAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
